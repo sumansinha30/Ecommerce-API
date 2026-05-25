08d99f2bf7af362af4807ebec9d4a7ca567ecc5e
sumansinha30 <sumantasinha34@gmail.com>
2026-05-25 13:09:35 +0530

    Update DB config for TiDB Cloud compatibility (Port 4000 and SSL)

--- config/database.js
+++ config/database.js
@@ -7,8 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
-    logging: false, // Set to true if you want to see SQL queries in the console
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false // Required for some cloud providers like TiDB/DigitalOcean
+      }
+    }
   }
 );
 
